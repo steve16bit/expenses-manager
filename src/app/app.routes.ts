d29b4ff334dbe361603aa4ddf7fdea0a837950ad
steve <heystevenn@gmail.com>
2025-01-07 23:54:06 -0300

    structuring project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Expenses } from './components/expenses/expenses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'expenses', component: Expenses}
+];
